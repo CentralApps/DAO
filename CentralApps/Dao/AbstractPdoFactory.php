f40a6cd99833c04fd8cc15fb349fa773bc240031
Michael Peacock <mkpeacock@gmail.com>
2014-02-02 22:53:05 +0000

    Added support for a build from pdo statement with a callback, useful for join queries. Note: requires PHP 5.4+ so breaks compatability with PHP 5.3 and below

--- CentralApps/Dao/AbstractPdoFactory.php
+++ CentralApps/Dao/AbstractPdoFactory.php
@@ -5,15 +5,32 @@ abstract class AbstractPdoFactory extends AbstractFactory
 {
     protected function buildFromPdoStatement(\PdoStatement $statement)
     {
+        return $this->buildFromPdoStatementLogic($statement, null);
+    }
+
+    protected function buildFromPdoStatementWithCallback(\PdoStatement $statement, callable $callback)
+    {
+        return $this->buildFromPdoStatementLogic($statement, $callback);
+    }
+
+    protected function buildFromPdoStatementLogic(\PdoStatement $statement, callable $callback = null)
+    {
         $collection = $this->getCollection();
         $statement->execute();
 
         while ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
             $model = new $this->modelClass($this->container);
+
             if (!($model instanceof ModelInterface)) {
                 throw new \LogicException("The abstract factory only supports models which implement ModelInterface and have a hydrate method");
             }
+
             $model->setExistsInDatabase(true);
+
+            if (!is_null($callback)) {
+                $model = call_user_func_array($callback, array($model, &$row));
+            }
+
             $model->hydrate($row);
             $collection->add($model);
         }
