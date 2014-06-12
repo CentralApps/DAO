5d3182dd8713fb5dd15ad5ee5e37864bd0fdf431
Michael Peacock <mkpeacock@gmail.com>
2014-06-12 15:17:43 +0100

    Update StockPdoFactoryTrait.php

--- CentralApps/Dao/StockPdoFactoryTrait.php
+++ CentralApps/Dao/StockPdoFactoryTrait.php
@@ -3,7 +3,6 @@ namespace CentralApps\Dao;
 
 trait StockPdoFactoryTrait
 {
-    protected $modelClass = null;
     protected $notFoundExceptionClass = '\\Exception';
     protected $activeTrait = '\\CentralApps\\Base\\Traits\\Active';
     protected $deletableTrait = '\\CentralApps\\Base\\Traits\\Deletable';
